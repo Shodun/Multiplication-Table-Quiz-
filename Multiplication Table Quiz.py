d06d9a1feab73549b4f9b72a685a9dfd520c135d
Shodun <81385241+Shodun@users.noreply.github.com>
2022-12-24 22:40:57 +0300

    Update Multiplication Table Quiz.py
    I am added negative numbers

--- Multiplication Table Quiz.py
+++ Multiplication Table Quiz.py
@@ -1,11 +1,11 @@
 import random
 while True:
-    list1=[3,4,6,7,8,9]
-    list2=[3,4,6,7,8,9]
+    list1=[3,4,6,7,8,9,-3,-4,-6,-7,-8,-9]
+    list2=[3,4,6,7,8,9,-3,-4,-6,-7,-8,-9]
     number1=random.choice(list1)
     number2=random.choice(list2)
     result=number1*number2
-    result2 = int(input(str(number1) + "x" + str(number2) + ":"))
+    result2 = int(input(str(number1) + " x " + str(number2) + ":"))
 
     if (result == result2):
 
