ec3cead73cb4594391dc1a95765f1e83ebe36636
Shodun <81385241+Shodun@users.noreply.github.com>
2023-01-01 15:22:06 +0300

    Update Multiplication Table Quiz.py

--- Multiplication Table Quiz.py
+++ Multiplication Table Quiz.py
@@ -1,8 +1,28 @@
-import random
+import random,time
 print("Made by SHODUN")
+
+Language= int(input("Please Select Language/Lütfen Dil Seçiniz 1.Türkçe/2.English: "))
+if Language==1:
+    Answer="Doğru Cevap: "
+    WrongAnswer="Yanlış Cevap: "
+    Negatif="Negatif Sayı Olsun mu 1.Evet/2.Hayır: "
+    Valid="Lütfen Geçerli Bir Şey giriniz "
+
+elif Language==2:
+    Answer = "Correct Answer "
+    WrongAnswer = "Wrong Answer: "
+    Valid="Please enter something valid"
+    Negatif="Do you want negative numbers 1.Yes/2.No"
+Negative=int(input(Negatif))
+if Negative==1:
+    list1 = [3, 4, 6, 7, 8, 9, -3, -4, -6, -7, -8, -9]
+    list2 = [3, 4, 6, 7, 8, 9, -3, -4, -6, -7, -8, -9]
+
+elif Negative==2:
+    list1 = [3, 4, 6, 7, 8, 9, ]
+    list2 = [3, 4, 6, 7, 8, 9, ]
 while True:
-    list1=[3,4,6,7,8,9,-3,-4,-6,-7,-8,-9]
-    list2=[3,4,6,7,8,9,-3,-4,-6,-7,-8,-9]
+
     number1=random.choice(list1)
     number2=random.choice(list2)
     result=number1*number2
@@ -10,9 +30,9 @@ while True:
 
     if (result == result2):
 
-        print("Correct answer")
+        print(Answer)
     else:
-        print("Wrong answer: "+str(result))
+        print(str(WrongAnswer)+str(result))
     continue
 
 
